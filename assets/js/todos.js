8537ce638846d460f83e9799c7a7f61e974a7386
maxkram <max.kramer@live.com>
2017-02-25 13:27:51 +0300

    1st commit
    basic html css js

---
+++ assets/js/todos.js
@@ -0,0 +1,22 @@
+//вычеркнуть отдельную задачу кликом
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//клик по ’ удал€ет задачу
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(event){
+	if(event.which === 13){
+		//подцепит новую задачу из окна ввода
+		var todoText = $(this).val();
+		$(this).val("");
+		//создаст новый li и ul
+		$("ul").append("<li><span></span> " + todoText + "</li>");
+	}
+});
